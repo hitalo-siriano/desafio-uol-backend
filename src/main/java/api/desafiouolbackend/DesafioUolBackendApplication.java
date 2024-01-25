458994c86003ed1cff2054c6b026a3827c00c470
Hitalo Siriano <hitalo.siriano@gmail.com>
2024-01-24 21:41:11 -0300

    first commit

---
+++ src/main/java/api/desafiouolbackend/DesafioUolBackendApplication.java
@@ -0,0 +1,13 @@
+package api.desafiouolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUolBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/desafiouolbackend/DesafioUolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package api.desafiouolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
