6081ffe520eabaab233742e6a98f96738d028990
Hitalo Siriano <hitalo.siriano@gmail.com>
2024-02-11 16:08:16 -0300

    delete e update funcionando , api pronta para ser consumida.

--- src/main/java/api/desafiouolbackend/controllers/PlayerController.java
+++ src/main/java/api/desafiouolbackend/controllers/PlayerController.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import org.hibernate.JDBCException;
 
@@ -17,6 +18,7 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,7 +29,10 @@ import api.desafiouolbackend.repositorys.PlayerRepository;
 
 import api.desafiouolbackend.services.PlayerService;
 import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -74,4 +79,41 @@ public class PlayerController {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		}
 	}
+	
+		
+		
+	@PutMapping("/api/update/player/{id}")
+	public ResponseEntity<Object> updatePlayer(@PathVariable(value = "id") UUID id,@RequestBody @Valid PlayerNewDto playerNewDto) throws Exception {
+
+		var optionalUser = playerService.updatePlayers(id);
+		if (optionalUser.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+
+		
+
+        
+        
+
+
+			return ResponseEntity.status(HttpStatus.OK).body(playerService.salvePlayer(playerNewDto,optionalUser.get().getIdPlayer()));
+		}
+	@DeleteMapping("/api/delete/player/{id}")
+	public ResponseEntity<Object> deletePlayer(@PathVariable(value = "id") UUID id){
+
+		
+         if(playerService.deletePlayers(id)) {
+        	 return ResponseEntity.status(HttpStatus.OK).body("successfully deleted");
+         }else {
+        	 return ResponseEntity.status(HttpStatus.NOT_FOUND).body("id not found");
+         }
+         
+		
+    
+        
+        
+
+
+			
+		}
 }
--- src/main/java/api/desafiouolbackend/services/PlayerService.java
+++ src/main/java/api/desafiouolbackend/services/PlayerService.java
@@ -2,6 +2,8 @@ package api.desafiouolbackend.services;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,9 +61,70 @@ public class PlayerService {
 		}
 
 	}
+	public PlayerModel salvePlayer(PlayerNewDto playerNewDto,UUID id) throws Exception {
+
+		if (playerNewDto.grouphero().equalsIgnoreCase("avengers")) {
+			String heroNickerNameValid = nickNameService.validHeroAvengers();
+			if (heroNickerNameValid == "") {
+				return new PlayerModel();
+			}
+			PlayerModel playerModel = new PlayerModel();
+			playerModel.setIdPlayer(id);
+			playerModel.setEmail(playerNewDto.email());
+			playerModel.setFone(playerNewDto.fone());
+			playerModel.setName(playerNewDto.name());
+			playerModel.setGrouphero(playerNewDto.grouphero());
+
+			playerModel.setNickname(heroNickerNameValid);
+
+			return playerRepository.save(playerModel);
+
+		} else if (playerNewDto.grouphero().equalsIgnoreCase("justice league")) {
+			String heroNickerNameValid = nickNameService.validHeroJustice();
+			if (heroNickerNameValid == "") {
+				return new PlayerModel();
+			}
+			PlayerModel playerModel = new PlayerModel();
+			playerModel.setIdPlayer(id);
+			playerModel.setEmail(playerNewDto.email());
+			playerModel.setFone(playerNewDto.fone());
+			playerModel.setName(playerNewDto.name());
+			playerModel.setGrouphero(playerNewDto.grouphero());
+
+			playerModel.setNickname(heroNickerNameValid);
+
+			return playerRepository.save(playerModel);
+
+		} else {
+			return null;
+		}
+
+	}
 	
 	public List <Object> getUser() {
 		
 		return  playerRepository.viewPlayer();
 	}
+	public Optional<PlayerModel> updatePlayers(UUID id){
+		
+	
+		return playerRepository.findById(id);
+	}
+	
+	public boolean deletePlayers(UUID id) {
+		
+		var optionalUser =playerRepository.findById(id);
+		
+		if (optionalUser.isEmpty()) {
+			return  false;
+		}else {
+			playerRepository.deleteById(id);
+			return true;
+		}
+		
+		
+		
+	}
+	
+	
 }
